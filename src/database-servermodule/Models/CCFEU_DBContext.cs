dee8a50a87cec19315b9bd7b00c430e7459f5b73
Krystof Spiller <253812@via.dk>
2019-11-15 08:57:26 +0100

    CC-10 Removed warnings

--- src/database-servermodule/Models/CCFEU_DBContext.cs
+++ src/database-servermodule/Models/CCFEU_DBContext.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace database_servermodule.Models
 {
-    public partial class CCFEU_DBContext : DbContext
+    public class CCFEU_DBContext : DbContext
     {
         public CCFEU_DBContext()
         {
